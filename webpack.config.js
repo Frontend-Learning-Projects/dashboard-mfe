215a053c4c5206c4ded69e5cc076407fe353b548
Muhammad-Usama-Mujeeb <usama.mujeeb@emumba.com>
2025-06-26 01:56:18 +0500

    chore: update package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,35 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    resolve: {
+      ...defaultConfig.resolve,
+      fullySpecified: false,
+      extensions: [
+        ...(defaultConfig.resolve && defaultConfig.resolve.extensions
+          ? defaultConfig.resolve.extensions
+          : []),
+        ".js",
+        ".mjs",
+      ],
+      alias: {
+        ...(defaultConfig.resolve && defaultConfig.resolve.alias
+          ? defaultConfig.resolve.alias
+          : {}),
+      },
+    },
+    module: {
+      ...defaultConfig.module,
+      rules: [
+        ...(defaultConfig.module && defaultConfig.module.rules
+          ? defaultConfig.module.rules
+          : []),
+        {
+          test: /\.m?js$/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+      ],
+    },
   });
 };
